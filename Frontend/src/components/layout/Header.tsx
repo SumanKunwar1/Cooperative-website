38716ac4e72b95465bb3c0c92efc5216e163683d
Suman Kunwar <sumankunwar755@gmail.com>
2025-12-07 11:56:14 +0545

    header updated

--- Frontend/src/components/layout/Header.tsx
+++ Frontend/src/components/layout/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
   const socialMedia = [
     {
       name: "Facebook",
-      url: "https://www.facebook.com/constellationcooperative1",
+      url: "https://www.facebook.com/constellation.cooperative.2025",
       icon: "https://img.freepik.com/premium-vector/round-facebook-logo-isolated-white-background_469489-897.jpg?semt=ais_hybrid&w=740&q=80",
     },
     {
